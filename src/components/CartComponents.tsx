2eaab20686051833c2bdb049115cea340fcdfa4c
valerio moscetti <valemosce2@gmail.com>
2025-07-21 15:42:22 +0200

    carrello vuotto e css cart

--- src/components/CartComponents.tsx
+++ src/components/CartComponents.tsx
@@ -14,25 +14,26 @@ export function CartCard({product}:{product:CartItem}) {
 
   return (
     <div className="flex flex-col sm:flex-row sm:items-start sm:space-x-4 p-4 border rounded-md w-full max-w-screen-md mx-auto">
-      <div className="sm:w-1/3 w-full mb-4 sm:mb-0">
-        <img src={product.image} alt={product.title} className="w-full h-auto object-cover rounded" />
+      <div className="sm:w-1/3 w-full mb-4 sm:mb-0 flex justify-center">
+        <img src={product.image} alt={product.title} className="w-30 h-auto object-cover rounded" />
       </div>
-      <div className="sm:w-2/3 w-full flex flex-col justify-between space-y-4">
+      <div className="sm:w-2/3 w-full flex flex-col justify-between space-y-8">
         <p className="text-lg font-semibold">{product.title} | €{product.price}</p>
         <div className="flex justify-between text-sm">
           <span>Quantità: {product.quantity}</span>
           <span>Prezzo Totale: €{(product.price * product.quantity).toFixed(2)}</span>
+          <span></span>
         </div>
         <div className="flex space-x-2">
-          <button
+          <Button
           onClick={()=>addToCart(product, "add")}
-          className="px-3 py-1 bg-green-500 text-white rounded hover:bg-green-600">+</button>
-          <button 
+          className="px-3 py-1 bg-black text-white rounded hover:bg-green-600 w-10">+</Button>
+          <Button 
           onClick={()=>addToCart(product, "reduce")}
-          className="px-3 py-1 bg-yellow-500 text-white rounded hover:bg-yellow-600">-</button>
-          <button 
+          className="px-3 py-1 bg-black text-white rounded hover:bg-yellow-600 w-10">-</Button>
+          <Button 
           onClick={()=>removeFromCart(product.id)}
-          className="px-3 py-1 bg-red-500 text-white rounded hover:bg-red-600">Rimuovi</button>
+          className="px-3 py-1 bg-red-900 text-white rounded hover:bg-red-600">Rimuovi</Button>
         </div>
       </div>
     </div>
--- src/pages/cart.tsx
+++ src/pages/cart.tsx
@@ -3,6 +3,7 @@ import type { Product } from "@/types/product";
 import type { CartItem } from "@/types/cart";
 import { useEffect, useState } from "react";
 import { useCart } from "@/context/CartContext";
+import { Link } from "react-router-dom";
 
 
 
@@ -12,7 +13,27 @@ export function Cart() {
   const { cart, addToCart, removeFromCart, clearCart } = useCart()
   const cartData = localStorage.getItem("cart")
   const [products, setProducts] = useState<CartItem[]>(cartData ? JSON.parse(cartData) : [])
+
   
+  const emptyOrNot=(products:CartItem[], option:"card" | "table")=>{
+    if(products.length){
+      if(option==="card"){
+        return (products.map((p, index) => (<CartCard key={index} product={p} />)))
+      }else{
+        return (<div className="w-full sm:max-w-sm sm:mx-auto  sticky top-[20px]"><Resoconto products={cart} /></div>)
+      }
+    }else{
+      if(option==="card"){
+        return( <div className="w-full flex justify-center-safe">
+          <div className="flex-wrap p-20">
+            <h2 className="font-extrabold text-3xl text-red-900">Il carrello è vuoto</h2>
+            <p>Vai alla <Link to={"/"} className="underline decoration-sky-500">Home</Link> e vedi i nostri prodotti</p>
+          </div>
+     </div>)
+      }
+
+    }
+  }
   useEffect(()=>{
 
     setProducts(cart)
@@ -20,14 +41,10 @@ export function Cart() {
 
   return (
     <div className="flex flex-col lg:flex-row justify-center items-start gap-8 p-6">
-      <div className="w-full lg:w-2/3 space-y-4">
-        {products.map((p, index) => (
-          <CartCard key={index} product={p} />
-        ))}
+      <div className="w-full lg:w-2/3 space-y-4 top-[20px] ">
+        {emptyOrNot(products, "card")}
       </div>
-      <div className="w-full sm:max-w-sm sm:mx-auto p-4">
-        <Resoconto products={cart} />
-      </div>
+      {emptyOrNot(products,"table")}
     </div>
   );
 }
