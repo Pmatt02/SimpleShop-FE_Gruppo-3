e8c5b939e75fe73ad24ea58678dd638db9519eda
John Benedict Yriarte <johnbenedictyriarte0303@gmail.com>
2025-07-23 14:40:57 +0200

    fixed navbar and form layout

--- src/pages/CheckoutPage.tsx
+++ src/pages/CheckoutPage.tsx
@@ -1,19 +1,17 @@
-import { CheckoutForm } from "@/components/checkout-form/CheckoutForm"
-import { Navbar } from "@/components/Navbar"
-import { Toaster } from "@/components/ui/sonner"
+import { CheckoutForm } from "@/components/checkout-form/CheckoutForm";
+import { Navbar } from "@/components/Navbar";
+import { Toaster } from "@/components/ui/sonner";
 
 export const CheckoutPage = () => {
-
-
-    return (
-        <>
-        <Navbar/>
-        <main className="flex items-center justify-center min-h-screen w-screen py-4">
-            
-            <CheckoutForm/>
-            <Toaster position="top-center"/>
-        </main>
-        </>
-
-    )
-}
+  return (
+    <>
+      <main className="w-full max-w-screen flex flex-col min-h-screen h-full">
+        <Navbar />
+        <div className="w-full grow py-5 h-max flex items-center justify-center">
+          <CheckoutForm />
+        <Toaster position="top-center" />
+        </div>
+      </main>
+    </>
+  );
+};
